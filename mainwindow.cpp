cd0fe7952162fdab4aab3d185a056fa3e2a6a8cc
cpedrohenrique1 <88724009+cpedrohenrique1@users.noreply.github.com>
2023-08-30 08:52:11 -0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,16 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->tableWidget->setColumnCount(5);
+    QStringList cabecalho = {"Matricula", "Nome", "Departamento", "Titulacao", "Tipo de contato"};
+    ui->tableWidget->setHorizontalHeaderLabels(cabecalho);
+    ui->tableWidget->setHorizontalHeaderLabels(cabecalho);
+    ui->tableWidget->setColumnWidth(0, 150); // Matricula
+    ui->tableWidget->setColumnWidth(1, 250); // Nome
+    ui->tableWidget->setColumnWidth(2, 100); // Departamento
+    ui->tableWidget->setColumnWidth(3, 100);   // Titulacao
+    ui->tableWidget->setColumnWidth(4, 150); // Tipo de contato
+    ui->tableWidget->setEditTriggers(QAbstractItemView::NoEditTriggers);
 }
 
 MainWindow::~MainWindow()
