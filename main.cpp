801213da13683b02663f55a5066d89c1b4974a74
cpedrohenrique1 <88724009+cpedrohenrique1@users.noreply.github.com>
2023-08-31 11:56:49 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,3 +9,41 @@ int main(int argc, char *argv[])
     w.show();
     return a.exec();
 }
+
+
+#include <iostream>
+
+void eita(int **nmr){
+    std::cout << **nmr << std::endl;
+    std::cout << *&nmr << std::endl;
+    std::cout << *nmr << std::endl;
+    *nmr = new int(50);
+}
+
+void functin(int **nmr){
+    std::cout << **nmr << std::endl;
+    std::cout << *&nmr << std::endl;
+    std::cout << *nmr << std::endl;
+    eita(nmr);
+}
+void anao(int &nmr){
+    std::cout << nmr << std::endl;
+    std::cout << &nmr << std::endl;
+    nmr = 5;
+}
+
+int main(){
+    int *nmr = new int(90);
+    std::cout << *nmr << std::endl;
+    std::cout << nmr << std::endl;
+    std::cout << &nmr << std::endl;
+    functin(&nmr);
+    std::cout << *nmr << std::endl;
+    std::cout << nmr << std::endl;
+    std::cout << &nmr << std::endl;
+    functin(&nmr);
+    // int numero = 20;
+    // anao(numero);
+    // std::cout << numero << std::endl;
+    // std::cout << &numero << std::endl;
+}
